82c7c67502f4121405affb97de711eaacd1ee59b
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2024-02-23 01:37:40 +0100

    Add new files and configurations for Tailwind CSS and Vite

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import navbar from './components/Navbar.vue';
+import ArmorSpeedComponent from './components/ArmorSpeedComponent.vue';
+</script>
+
+<template>
+  <navbar />
+  <ArmorSpeedComponent />
+  <div class="divider divider-secondary font-bold">Attributes</div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ArmorSpeedComponent.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue'
+import monster from '../assets/werebear.json'
+</script>
+
+<template>
+    <!-- <div class="stats shadow-xl flex">
+        <div class="stat" v-for="ac_elem in monster.ac">
+            <div class="stat-title">Armor Class</div>
+            <div class="stat-desc">
+                <div v-if="ac_elem.from">{{ac_elem.from[0]}}</div>
+                <div v-else>-</div>
+            </div>
+            <div class="stat-value text-primary text-2xl">{{ac_elem.ac}}</div>
+            <div class="stat-desc">
+                <div v-if="ac_elem.condition">{{ac_elem.condition}}</div>
+                <div v-else>-</div>
+            </div>    
+        </div>
+        <div class="stat" v-for="(item, key ) in monster.speed">
+            <div class="stat-title">Speed</div>
+            <div class="stat-desc">{{key}}</div>
+            <div class="stat-value text-primary text-2xl">
+                <div v-if="item.number">{{item.number}} ft.</div>
+                <div v-else>{{item}} ft.</div>
+            </div>
+            <div class="stat-desc">
+                <div v-if="item.number">{{item.condition}}</div>
+                <div v-else>-</div>
+            </div>
+        </div>
+    </div> -->
+    <div class="flex w-full">
+        <p class="font-bold">Armor Class:&nbsp;</p>
+        <div v-for="ac_elem in monster.ac">
+            <span class="text-primary font-bold">{{ac_elem.ac}}&nbsp;</span>
+
+            <span v-if="ac_elem.from">({{ac_elem.from[0]}})&nbsp;</span>
+
+            <span v-if="ac_elem.condition">{{ac_elem.condition}};&nbsp;</span>
+        </div>
+    </div>
+
+    <div class="flex w-full">
+        <p class="font-bold">Speed:&nbsp;</p>
+        <div v-for="(item, key) in monster.speed">
+            <span class="">{{key}}&nbsp;</span>
+
+            <span class="text-primary font-bold" v-if="item.number">{{item.number}} ft.&nbsp;</span>
+            <span class="text-primary font-bold" v-else>{{item}} ft.&nbsp;</span>
+
+            <span v-if="item.number">{{item.condition}};&nbsp;</span>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
