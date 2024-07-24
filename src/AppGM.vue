2720c1a7fbff6be45d177ccbaa9834a18f20182f
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2024-07-24 19:27:31 +0200

    add clash_id and clash_iniative

--- src/AppGM.vue
+++ src/AppGM.vue
@@ -137,6 +137,9 @@ const confirmTokenUpdate = () => {
                         item.metadata[`${CLASH_ID}/clash_currentHP`] = selectedMonster.value.hp.average;
                         item.metadata[`${CLASH_ID}/clash_dexSave`] = Math.floor((selectedMonster.value.dex - 10) / 2);
                         item.metadata[`${CLASH_ID}/clash_dexScore`] = selectedMonster.value.dex
+                        item.metadata[`${CLASH_ID}/clash_initiative`] = 10 + Math.floor((selectedMonster.value.dex - 10) / 2);
+                        // Set Clash ID to have a the metadata applied directly
+                        item.metadata[`${CLASH_ID}/clash_id`] = item.id;
                     }
                 });
             });
